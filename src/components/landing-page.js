03f78ddf6ec36576ae7518c83321380018656daa
Owcollier <owcollier@gmail.com>
2018-02-06 20:04:06 -0500

    added a ton of filler text to the landing page. Goofed around

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -12,9 +12,23 @@ export function LandingPage(props) {
 
     return (
         <div className="home">
-            <h2>Welcome to Foo App</h2>
+            <h2>Welcome to French Flash Cards!</h2>
             <div className="app-description">
-                <span>Lorem ipsum dolor sit amet, massa faucibus ex vulputate volutpat, lacus imperdiet ac. Nisl ut, arcu et, magnis cras at, suscipit posuere, leo at. Aliquam risus mus suspendisse aliquam id, laoreet metus mattis amet sed mauris, convallis neque ligula condimentum nulla. Et ullamcorper nunc.</span>
+                <span className="landing-descrip"><strong>French Flash Cards is an application designed to help English speakers learn basic French vocabulary via Spaced Repetition based exercises.</strong></span>
+                <br></br>
+                <br></br>
+                <span className="landing-descrip">Spaced repetition is a method for efficient learning that has you practice concepts or skills over increasing periods of time. It's based on the notion of a "forgetting curve," or the idea that over time, if we don't actively use or reflect on something we know, our knowledge decays. With spaced repetition, we stay ahead of that moment of forgetting, but we do it in a smart way: if we know something, we don't need to practice it for some period of time. If we don't know something, we do need to practice it.</span>
+                <br></br>
+                <br></br>
+                <span className="landing-descrip"><strong>French Flash Cards</strong> implements the Spaced Repetition method by allowing you to practice French Vocabulary according to its principles. A practice session revolves around a set of vocabulary words served up to you according to which word is at the beginning or <em>head</em> of a vocabulary list. Every word that you translate correctly will be pushed further back in the list, while every word you translate incorrectly will remain near the beginning of the list. As a result, you will be provided with the opportunity to review what you don't know more often until you master it, and revisit what you do know less often in order to test how well you've retained the knowledge!</span>
+                <br></br>
+                <br></br>
+                <span className="landing-descrip">We at <strong>Lingua Franc</strong> trust that you will enjoy using <strong>French Flash Cards</strong> for your French vocabulary practice and wish you both a wonderful life and <em>fantastique</em> day</span>
+                <br></br>
+                <br></br>
+                <span className="landing-page-adieu">Au revior and adieu!</span>
+                <br></br>
+                <br></br>
             </div>
             <LoginForm />
             <Link to="/register">Register</Link>
--- src/components/login-form.js
+++ src/components/login-form.js
@@ -19,32 +19,35 @@ export class LoginForm extends React.Component {
             );
         }
         return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                {error}
-                <label htmlFor="username">Username</label>
-                <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    id="username"
-                    validate={[required, nonEmpty]}
-                />
-                <label htmlFor="password">Password</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    validate={[required, nonEmpty]}
-                />
-                <button disabled={this.props.pristine || this.props.submitting}>
-                    Log in
-                </button>
-            </form>
+            <div className="login-container">
+            <span className="login-span">Log in below if you already have an account. Otherwise, please click register to sign up!</span>
+                <form
+                    className="login-form"
+                    onSubmit={this.props.handleSubmit(values =>
+                        this.onSubmit(values)
+                    )}>
+                    {error}
+                    <label htmlFor="username">Username</label>
+                    <Field
+                        component={Input}
+                        type="text"
+                        name="username"
+                        id="username"
+                        validate={[required, nonEmpty]}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <Field
+                        component={Input}
+                        type="password"
+                        name="password"
+                        id="password"
+                        validate={[required, nonEmpty]}
+                    />
+                    <button disabled={this.props.pristine || this.props.submitting}>
+                        Log in
+                    </button>
+                </form>
+            </div>
         );
     }
 }
